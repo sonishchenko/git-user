0113cabdcb683fbe55934bbd721aa7821f764287
sonishchenko <HDPChdpc96945763>
2019-10-03 13:26:13 +0300

    array

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {UserDetails} from "../user-details/user-details";
 
 
 import './App.css';
 
 
-function App() {
-  return (
-    <div className="all-users">
-        <div>
-        <UserDetails />
-        </div>
-        <div>
-            <UserDetails />
-        </div>
-    </div>
+export default class App extends Component {
 
-  );
+render() {
+    return (
+        <UserDetails />
+
+    )
 }
 
-export default App;
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './components/app/App';
 import * as serviceWorker from './serviceWorker';
 
+fetch('https://api.github.com/search/users?q=location:kiev&sort=followers&order=desc&per_page=10&page=1')
+    .then((res) => {
+        return res.json();
+    })
+    .then((body) => {
+       console.log(body);
+    });
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
