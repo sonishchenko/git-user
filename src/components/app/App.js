e4b943efd8ae9a47a2cb44e3c1521ac0a0c78607
sonishchenko <HDPChdpc96945763>
2019-10-03 13:41:45 +0300

    sorry)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,15 +5,3 @@ import {UserDetails} from "../user-details/user-details";
 import './App.css';
 
 
-export default class App extends Component {
-
-render() {
-    return (
-        <UserDetails />
-
-
-
-    )
-}
-
-}
