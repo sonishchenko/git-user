efbe5e8c90a7a641935c01ce9b8ff4b3b509c513
sonishchenko <HDPChdpc96945763>
2019-12-12 20:30:50 +0200

    add swich case in reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,25 @@
 import { createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
-const reducer = (users, action) {
-    
+const reducer = (users, action) => {
+    switch (action.type) {
+    case 'FETCH_USERS_START':
+        return true;
+    case 'FETCH_USERS_SUCCESS':
+        return false ;   
+    case 'FETCH_USERS_ERROR':
+        return false ;  
+     default:
+         return users   
 }
+};
 
-const store = createStore(reducer(thunkMiddleware) ) 
+const store = createStore(reducer(thunkMiddleware) );
+
+const fetchUsersStart = () => {({ type: FETCH_USERS_START })};
+
+const fetchUsersSuccess = () => {({ type: FETCH_USERS_SUCCESS })};
+
+const fetchUsersError = () => {({ type: FETCH_USERS_ERROR })};
 
 export default store
