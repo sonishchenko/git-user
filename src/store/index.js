c3d14f247b962ceda5a77dbd0c3fcb073e32f3cb
sonishchenko <HDPChdpc96945763>
2020-01-24 12:09:26 +0200

    Create action and reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,19 @@
 import { createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
-const reducer = (users, action) => {
+const reducer = (state, action) => {
     switch (action.type) {
-    case fetching === true:
-        return 'FETCH_USERS_START';
-    case fetching === false:
-        return 'FETCH_USERS_SUCCESS' ;   
-    case fetching === false:
-        return 'FETCH_USERS_ERROR';  
+    case 'FETCH_USERS_START':
+        return { ...state, fetching: true};
+    case 'FETCH_USERS_SUCCESS':
+        return  { ...state, fetching: false};   
+    case 'FETCH_USERS_ERROR':
+        return { ...state, fetching: false};  
      default:
-         return users   
+         return state   
 }
 };
 
 const store = createStore(reducer(thunkMiddleware) );
-
-const fetchUsersStart = () => {({ type: FETCH_USERS_START })};
-
-const fetchUsersSuccess = () => {({ type: FETCH_USERS_SUCCESS })};
-
-const fetchUsersError = () => {({ type: FETCH_USERS_ERROR })};
 
 export default store
